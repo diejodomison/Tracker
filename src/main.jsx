ca7955fa48a118cafb649d5aac1bfdeef96facf7
diejodomison <ddiejo57@gmail.com>
2026-03-10 19:46:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.body.style.background = '#080C14';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
